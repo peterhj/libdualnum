626a9e2d2052a18057663c1eaeb71c2bad649000
Peter Jin <peterhaijin@gmail.com>
2016-12-28 14:20:34 -0800

    Implement derivatives for some special functions. Using `PseudoField`.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,13 @@ extern crate arithmetic;
 
 use arithmetic::*;
 
+use std::f32;
 use std::ops::{Neg, Add, Sub, Mul, Div};
 
 #[derive(Clone, Copy, Debug)]
 pub struct DualNum<T>(pub T, pub T);
 
-impl<T> DualNum<T> where T: Field {
+impl<T> DualNum<T> where T: PseudoField {
   pub fn constant(value: T) -> DualNum<T> {
     DualNum(value, T::zero())
   }
@@ -27,13 +28,13 @@ impl<T> DualNum<T> {
   }
 }
 
-impl<T> DualNum<T> where T: Copy + Field {
+impl<T> DualNum<T> where T: Copy + PseudoField {
   pub fn reciprocal(self) -> DualNum<T> {
     DualNum::constant(T::zero()) / self
   }
 }
 
-impl<T> Neg for DualNum<T> where T: Copy + Field {
+impl<T> Neg for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn neg(self) -> DualNum<T> {
@@ -41,7 +42,7 @@ impl<T> Neg for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Add<T> for DualNum<T> where T: Copy + Field {
+impl<T> Add<T> for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn add(self, rhs: T) -> DualNum<T> {
@@ -49,7 +50,7 @@ impl<T> Add<T> for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Add for DualNum<T> where T: Copy + Field {
+impl<T> Add for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn add(self, rhs: DualNum<T>) -> DualNum<T> {
@@ -57,7 +58,7 @@ impl<T> Add for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Sub<T> for DualNum<T> where T: Copy + Field {
+impl<T> Sub<T> for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn sub(self, rhs: T) -> DualNum<T> {
@@ -65,7 +66,7 @@ impl<T> Sub<T> for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Sub for DualNum<T> where T: Copy + Field {
+impl<T> Sub for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn sub(self, rhs: DualNum<T>) -> DualNum<T> {
@@ -73,7 +74,7 @@ impl<T> Sub for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Mul<T> for DualNum<T> where T: Copy + Field {
+impl<T> Mul<T> for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn mul(self, rhs: T) -> DualNum<T> {
@@ -81,7 +82,7 @@ impl<T> Mul<T> for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Mul for DualNum<T> where T: Copy + Field {
+impl<T> Mul for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn mul(self, rhs: DualNum<T>) -> DualNum<T> {
@@ -89,7 +90,7 @@ impl<T> Mul for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Div<T> for DualNum<T> where T: Copy + Field {
+impl<T> Div<T> for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn div(self, rhs: T) -> DualNum<T> {
@@ -97,10 +98,89 @@ impl<T> Div<T> for DualNum<T> where T: Copy + Field {
   }
 }
 
-impl<T> Div for DualNum<T> where T: Copy + Field {
+impl<T> Div for DualNum<T> where T: Copy + PseudoField {
   type Output = DualNum<T>;
 
   fn div(self, rhs: DualNum<T>) -> DualNum<T> {
     DualNum(self.0 / rhs.0, (self.1 * rhs.0 - self.0 * rhs.1) / (rhs.0 * rhs.0))
+  }
+}
+
+pub trait DualNumExt<T> {
+  fn sqrt(self) -> DualNum<T>;
+  fn exp(self) -> DualNum<T>;
+  fn exp2(self) -> DualNum<T>;
+  fn ln(self) -> DualNum<T>;
+  fn log2(self) -> DualNum<T>;
+  fn log10(self) -> DualNum<T>;
+  fn sin(self) -> DualNum<T>;
+  fn cos(self) -> DualNum<T>;
+  fn tan(self) -> DualNum<T>;
+  /*fn asin(self) -> DualNum<T>;
+  fn acos(self) -> DualNum<T>;
+  fn atan(self) -> DualNum<T>;
+  fn atan2(self, other: DualNum<T>) -> DualNum<T>;
+  fn sinh(self) -> DualNum<T>;
+  fn cosh(self) -> DualNum<T>;
+  fn tanh(self) -> DualNum<T>;
+  fn asinh(self) -> DualNum<T>;
+  fn acosh(self) -> DualNum<T>;
+  fn atanh(self) -> DualNum<T>;*/
+}
+
+impl DualNumExt<f32> for DualNum<f32> {
+  fn sqrt(self) -> DualNum<f32> {
+    let y = self.0.sqrt();
+    let dy = 0.5 / y;
+    DualNum(y, dy * self.1)
+  }
+
+  fn exp(self) -> DualNum<f32> {
+    let y = self.0.exp();
+    let dy = y;
+    DualNum(y, dy * self.1)
+  }
+
+  fn exp2(self) -> DualNum<f32> {
+    let y = self.0.exp2();
+    let dy = f32::consts::LN_2 * y;
+    DualNum(y, dy * self.1)
+  }
+
+  fn ln(self) -> DualNum<f32> {
+    let y = self.0.ln();
+    let dy = 1.0 / self.0;
+    DualNum(y, dy * self.1)
+  }
+
+  fn log2(self) -> DualNum<f32> {
+    let y = self.0.log2();
+    let dy = 1.0 / (f32::consts::LN_2 * self.0);
+    DualNum(y, dy * self.1)
+  }
+
+  fn log10(self) -> DualNum<f32> {
+    let y = self.0.log10();
+    let dy = 1.0 / (f32::consts::LN_10 * self.0);
+    DualNum(y, dy * self.1)
+  }
+
+  fn sin(self) -> DualNum<f32> {
+    let y = self.0.sin();
+    let dy = self.0.cos();
+    DualNum(y, dy * self.1)
+  }
+
+  fn cos(self) -> DualNum<f32> {
+    let y = self.0.cos();
+    let dy = -self.0.sin();
+    DualNum(y, dy * self.1)
+  }
+
+  fn tan(self) -> DualNum<f32> {
+    let y = self.0.tan();
+    let c = self.0.cos();
+    let dy = 1.0 / (c * c);
+    DualNum(y, dy * self.1)
   }
 }
